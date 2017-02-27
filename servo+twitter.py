20490569948246d105ce22fd4f335d6a8d82e862
ranjinipnarayan <ranjini.padma.narayan@gmail.com>
2017-02-27 16:52:37 -0500

    help it

--- servo+twitter.py
+++ servo+twitter.py
@@ -1,6 +1,5 @@
 import time
 import os
-from Tkinter import *
 import RPi.GPIO as GPIO
 from twython import TwythonStreamer
 
@@ -11,6 +10,8 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(SERVO, GPIO.OUT)
 pwm = GPIO.PWM(SERVO,100)
 pwm.start(5)
+
+ss = ServoSix()
 
 # Search terms
 TERMS = '#trump, #immigration, #wall, #prolife #prochoice'
@@ -33,7 +34,7 @@ class ServoSix:
     os.system("sudo /etc/init.d/servoblaster start")
         
     
-  def set_servo(self, servo, angle):
+  def set_servo(self, angle):
     if angle < 0 or angle > 180:
         print("Angle 0 to 180")
     else:
@@ -41,18 +42,18 @@ class ServoSix:
         scaler = range / 180
         pulse = self.servo_min + angle * scaler
         self.current_angle = self.servo_min + angle * scaler
-        command = "echo {}={}us > /dev/servoblaster".format(servo, pulse)
+        command = "echo {}={}us > /dev/servoblaster".format(6, pulse)
         os.system(command)
             
             
-  def move_servo(self, servo, start_angle, end_angle, delay):
+  def move_servo(self,  start_angle, end_angle, delay):
     inc = 1
     angle = start_angle
     if start_angle > end_angle:
       inc = -1
     while angle != end_angle:
       angle += inc
-      self.set_servo(servo, angle)
+      self.set_servo(angle)
       time.sleep(delay)
             
   def cleanup(self):
@@ -60,18 +61,6 @@ class ServoSix:
     os.system("sudo /usr/bin/killall servod")
     # disable it so it doesn't start after reboot'
     os.system("sudo update-rc.d servoblaster disable")  
-        
-# class App:
-#   def __init__(self, master):
-#     frame = Frame(master)
-#     frame.pack()
-#     scale = Scale(frame, from_=0, to=180,
-#         orient=HORIZONTAL, command=self.update)
-#     scale.grid(row=0)
-      
-  def update(self, angle):
-    duty = float(angle) / 10.0 + 2.5
-    pwm.ChangeDutyCycle(duty)
         
 # Setup callbacks from Twython Streamer
 class Twitter2RaspberryPi(TwythonStreamer):
@@ -83,12 +72,11 @@ class Twitter2RaspberryPi(TwythonStreamer):
       #   ss.move_servo(ss.current_angle, 50, 10)
       # elif 'prochoice' in data or 'prolife' in data: 
       #   print data['text'].encode('utf-8')
-        ss.move_servo(ss.current_angle, 100, 10)
-        delay(100)
-
+      ss.move_servo(ss.current_angle, 100, 10)
 
   def on_error(self, status_code, data):
     print status_code, data
+
 # Create streamer
 try:
   ss.set_servo(0):
@@ -98,11 +86,10 @@ except KeyboardInterrupt:
   GPIO.cleanup()
   ss.cleanup()
 
-root = Tk()
-root.wm_title('Servo Control')
-app = App(root)
-root.geometry("200x50+0+0")
-root.mainloop()
-ss = ServoSix()
+# root = Tk()
+# root.wm_title('Servo Control')
+# app = App(root)
+# root.geometry("200x50+0+0")
+# root.mainloop()
 
 
